901b5dbdc2068eca6445f7796f128f4bef82faf7
Kaynan Lima <xbhuecrazy@gmail.com>
2026-08-11 23:51:30 -0300

    Exercício de calculo
    calculo de soma e divisão

---
+++ calculos .cpp
@@ -0,0 +1,33 @@
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	float numero1, numero2, numero3, numero4, resultado;
+	cout<< "digite o numero1 ";
+	cin>>numero1;
+	cout<< "digite o numero2 ";
+	cin>>numero2;
+	cout<< "digite o numero3 ";
+	cin>>numero3;
+
+	numero4=numero1+numero2;
+
+	if (numero3==0)
+	{
+	  cout<< "nunca dividiras por zero";
+	}
+
+
+    else
+	{
+	resultado=numero4/numero3;
+	cout<<"resultado final = "<<resultado;
+
+	}
+	
+
+}
+
